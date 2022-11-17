24c6400ed3d11034ec83b9060af9986e22c14e2a
manikandanmohan10 <manikandan.mohan@softsuave.com>
2022-11-17 15:53:17 +0530

    Register and Login

--- Learning/urls.py
+++ Learning/urls.py
@@ -34,6 +34,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema=swagger-ui'),
-    path('auth/', include('Test.urls')),
+    path('api/', include('Test.urls')),
     path('admin/', admin.site.urls),
 ]
--- Test/urls.py
+++ Test/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='index'),
-    path("register/", views.RegisterAPI.as_view(), name='register'),
-    path("login/", views.LoginAPI.as_view(), name='login')
+    path("v1/register/", views.RegisterAPI.as_view(), name='register'),
+    path("v1/login/", views.LoginAPI.as_view(), name='login')
 ]
