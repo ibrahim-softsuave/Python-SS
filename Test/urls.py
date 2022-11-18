e46f2d7a01de3ee0ecbfaa94a234c2f5b46c39a3
manikandanmohan10 <manikandan.mohan@softsuave.com>
2022-11-18 09:47:11 +0530

    Register and Login

--- Test/urls.py
+++ Test/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='index'),
-    path("v1/register/", views.RegisterAPI.as_view(), name='register'),
-    path("v1/login/", views.LoginAPI.as_view(), name='login')
+    path("v1/register", views.RegisterAPI.as_view(), name='register'),
+    path("v1/login", views.LoginAPI.as_view(), name='login'),
 ]
